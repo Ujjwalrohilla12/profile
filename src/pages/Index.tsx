aa59d27d906ef215df62634174fedb2ea1080e9f
Ujjwalrohilla12 <rohillaujjwal39@gmail.com>
2026-03-22 13:21:56 +0530

    second commit

---
+++ src/pages/Index.tsx
@@ -0,0 +1,29 @@
+import { Navbar } from "@/components/portfolio/Navbar";
+import { HeroSection } from "@/components/portfolio/HeroSection";
+import { AboutSection } from "@/components/portfolio/AboutSection";
+import { SkillsSection } from "@/components/portfolio/SkillsSection";
+import { ExperienceSection } from "@/components/portfolio/ExperienceSection";
+import { ProjectsSection } from "@/components/portfolio/ProjectsSection";
+import { CertificationsSection } from "@/components/portfolio/CertificationsSection";
+import { EducationSection } from "@/components/portfolio/EducationSection";
+import { ContactSection } from "@/components/portfolio/ContactSection";
+import { Footer } from "@/components/portfolio/Footer";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground">
+      <Navbar />
+      <HeroSection />
+      <AboutSection />
+      <SkillsSection />
+      <ExperienceSection />
+      <ProjectsSection />
+      <CertificationsSection />
+      <EducationSection />
+      <ContactSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
